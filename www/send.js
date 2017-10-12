fc7e65d71d1628a973c7bbd0b75b6e33e8478ebf
Ankur Lathwal <alathw2@uic.edu>
2017-10-11 23:54:12 -0500

    Implemented logic to populate template as per given data. Used moment.js for dates and time zones.

--- www/send.js
+++ www/send.js
@@ -1,16 +1,41 @@
-
-$("#send").click(function (){
+var message;
+$("#update").click(function (){
     
     var selGuest = $("#selectGuest option:selected").val();
     var selCompany = $("#selectCompany option:selected").val();
     var selTemplate = $("#selectTemplate option:selected").val();
 
-    var message = templates[selTemplate].message;
+    message = templates[selTemplate].message;
     var variables = templates[selTemplate].variables;
     var company = companies[selCompany];
     var guest = guests[selGuest];
 
+    
+
     for(var i=0;i<variables.length;i++){
+        if(message.indexOf("greeting")>=0){            
+            var hrs = moment().tz(company.timezone).format('H');
+            if(hrs<12){
+                message = message.replace("$greeting$","Good Morning");
+            }
+            else if(hrs>=12 && hrs<16){
+                message = message.replace("$greeting$","Good Afternoon");
+            }
+            else{
+                message = message.replace("$greeting$","Good Evening");
+            }
+        }
+
+        if(variables[i]==="endTimestamp"){
+            message = message.replace("$endTimestamp$", moment(guest.reservation.endTimestamp*1000).format("ddd, MMM D YYYY"));
+        }
+        if(variables[i]==="startTimestamp"){
+            message = message.replace("$startTimestamp$", moment(guest.reservation.startTimestamp*1000).format("ddd, MMM D YYYY"));
+        }
+        if(guest.reservation.hasOwnProperty(variables[i])){
+            message = message.replace("$" +variables[i]+"$",guest.reservation[variables[i]] );
+        }
+
         if(guest.hasOwnProperty(variables[i])){
             message = message.replace("$" +variables[i]+"$",guest[variables[i]] );
         }
@@ -19,8 +44,12 @@ $("#send").click(function (){
         }
     }
 
+//    console.log(moment().tz("US/Pacific").format('H'));
 
-
-    $(".container").append(message);
+    $("#messageInput").html(message);
     
+});
+
+$("#send").click(function(){
+    $("#messageSent").html("<b>The following message was sent:</b><br>"+message);
 });
